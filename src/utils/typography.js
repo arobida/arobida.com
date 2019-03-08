05a1be6cfb74efc96ff5edc2f5bdf48e0d44b739
arobida <afrobida@me.com>
2019-03-07 19:37:40 -0800

    fonts

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,6 +3,6 @@ import Lawton from 'typography-theme-lawton'
 import Air from './AirbnbCereal-Bold.ttf'
 import Ops from './OperatorSSm-Medium-Pro.otf'
 
-const typography = new Typography(Ops)
+const typography = new Typography(Lawton)
 
 export default typography
